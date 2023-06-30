bb062a6b32a18950e1d8ab333ea5c96d65e1c86d
RamPraSath <63347762+AstroRam@users.noreply.github.com>
2023-06-30 22:06:04 +0200

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,16 +2,17 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 // registering username and password
-// let users = require("./auth_users.js").users;
+let users = require("./auth_users.js").users;
 const public_users = express.Router();
-let users = []
+// let users = []
 
 public_users.post("/register", (req,res) => {
     const username = req.params.username;
     const password = req.params.password;
     if (username && password) {
-      if (username.length > 3) { 
-        users.create({"username":username,"password":password});
+      if (!doesExist(username)) { 
+        // res.send(` Username: ${username} Password: ${password}`)
+        users.push({"username":username, "password":password});
         return res.status(200).json({message: "User successfully registred. Now you can login"});
       } else {
         return res.status(404).json({message: "User already exists!"});
@@ -20,6 +21,13 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."})
     ;
 });  
+
+public_users.post('/login', (req, res) => {
+    // Insert Login Code Here
+    let username = req.params.username;
+    let password = req.params.password;
+    res.send(`Username: ${username} Password: ${password}`);
+});
 
 // Get the book list available in the shop
 public_users.get('/',(req, res)=>{
@@ -50,6 +58,7 @@ public_users.get('/title/:title', (req, res)=> {
   let filtered_title = books.filter((books)=>books.title===title);
   res.send(filtered_title)
 });
+
 // Get book details based on ISBN
 public_users.get('/reviews/:isbn', (req, res)=> {
     //Write your code here
@@ -58,7 +67,7 @@ public_users.get('/reviews/:isbn', (req, res)=> {
     let filtered_books = books.filter((books)=> books.isbn === isbn);
     let filtered_reviews = filtered_books.filter((filtered_books)=> filtered_books.reviews===reviews);
     res.send(filtered_reviews)
-  });  
+});  
 
 //   public_users.post("/register", (req,res) => {
 //     const username = req.params.username
